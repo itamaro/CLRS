c5e5c9b599f9a6855ad510dd69b4ad99a8734051
Itamar Ostricher <itamarost@gmail.com>
2018-01-24 14:16:12 +0200

    Implement linked list reversal also for singly linked list

--- Algorithms/ReverseLinkedList/go/reverse.go
+++ Algorithms/ReverseLinkedList/go/reverse.go
@@ -17,12 +17,26 @@ func Reverse(l *linkedlist.LinkedList) {
   l.Front, l.Back = l.Back, l.Front
 }
 
+// Reverse a singly linked list, in place, in O(N) runtime with O(1) memory
+func SReverse(l *linkedlist.SLinkedList) {
+  var prev, e *linkedlist.SListNode = nil, l.Head
+  for e != nil {
+    e.Next, prev, e = prev, e, e.Next
+  }
+  l.Head = prev
+}
+
 func main() {
   var l linkedlist.LinkedList
+  var sl linkedlist.SLinkedList
   for i := 0; i < 10; i++ {
     l.PushBack(i)
+    sl.Insert(i)
   }
-  fmt.Printf("List:     %v\n", l.AsSlice())
+  fmt.Printf("List:      %v\n", l.AsSlice())
   Reverse(&l)
-  fmt.Printf("Reversed: %v\n", l.AsSlice())
+  fmt.Printf("Reversed:  %v\n", l.AsSlice())
+  fmt.Printf("SList:     %v\n", sl.AsSlice())
+  SReverse(&sl)
+  fmt.Printf("SReversed: %v\n", sl.AsSlice())
 }
--- Algorithms/ReverseLinkedList/go/reverse_test.go
+++ Algorithms/ReverseLinkedList/go/reverse_test.go
@@ -10,7 +10,22 @@ import (
 
 func TestReverseList(t *testing.T) {
 	var l linkedlist.LinkedList
-	for i := 0; i < 1000; i++ {
+
+  // test nil case
+  Reverse(&l)
+  if l.Front != nil || l.Back != nil || l.Len != 0 {
+    t.Errorf("Reversing empty list did something weird!")
+  }
+
+  // test trivial case
+  e := l.PushBack(0)
+  Reverse(&l)
+  if l.Front != e || l.Back != e || l.Len != 1 {
+    t.Errorf("Reversing list with one node did something weird!")
+  }
+
+  // test useful case
+	for i := 1; i < 1000; i++ {
 		l.PushBack(i)
 	}
   front, back := l.Front, l.Back
@@ -24,5 +39,36 @@ func TestReverseList(t *testing.T) {
   }
   if !reflect.DeepEqual(l.AsSlice(), slice) {
     t.Errorf("Expected list slice to be reversed")
+  }
+}
+
+func TestReverseSList(t *testing.T) {
+  var l linkedlist.SLinkedList
+
+  // test nil case
+  SReverse(&l)
+  if l.Head != nil || l.Len != 0 {
+    t.Errorf("Reversing empty list did something weird!")
+  }
+
+  // test trivial case
+  e := l.Insert(0)
+  SReverse(&l)
+  if l.Head != e || l.Len != 1 {
+    t.Errorf("Reversing list with one node did something weird!")
+  }
+
+  // test useful case
+  for i := 1; i < 1000; i++ {
+    e = l.InsertAfter(e, i)
+  }
+  SReverse(&l)
+  if l.Len != 1000 {
+    t.Errorf("Expected reversed list to have length 1000, got %d", l.Len)
+  }
+  for i, e := 999, l.Head; i >= 0; i, e = i - 1, e.Next {
+    if i != e.Value.(int) {
+      t.Fatalf("List not reversed")
+    }
   }
 }
