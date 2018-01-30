2128fa8263483ca7b0129d16af169032ff530210
Itamar Ostricher <itamarost@gmail.com>
2018-01-30 10:37:12 +0200

    Add another MergeSort Go implementation using Goroutines

--- Algorithms/Sort/go/mergesort.go
+++ Algorithms/Sort/go/mergesort.go
@@ -3,6 +3,8 @@
 
 package sort
 
+import "sync"
+
 // Sort the given `slice` in-place (non-decreasing order) using the merge sort algorithm
 // Runs in O(NlgN) time with O(N) memory
 func MergeSort(slice []int) {
@@ -16,6 +18,28 @@ func MergeSort(slice []int) {
   merge(slice, q)
 }
 
+// Sort the given `slice` in-place (non-decreasing order) using the merge sort algorithm
+// and taking advantage of goroutines to do recursive calls on partial slices in parallel
+// Runs in O(NlgN) time with O(N) memory
+func GoMergeSort(slice []int) {
+  var wg sync.WaitGroup
+  wg.Add(1)
+  goMergeSortHelper(slice, &wg)
+}
+
+func goMergeSortHelper(slice []int, wg *sync.WaitGroup) {
+  defer wg.Done()
+  if len(slice) < 2 {
+    return
+  }
+  q := (len(slice) + 1) / 2
+  var subWg sync.WaitGroup
+  subWg.Add(2)
+  go goMergeSortHelper(slice[:q], &subWg)
+  go goMergeSortHelper(slice[q:], &subWg)
+  subWg.Wait()
+  merge(slice, q)
+}
 
 func merge(slice []int, q int) {
   tmp := make([]int, len(slice))
